5978f72de052cd25eda3db3b072ed08ef1fde578
SusanMackson <macksonebosetale@gmail.com>
2023-03-17 16:09:10 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ *
+ * Return: ALways 0 (Success)
+ */
+
+int main(void)
+
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
