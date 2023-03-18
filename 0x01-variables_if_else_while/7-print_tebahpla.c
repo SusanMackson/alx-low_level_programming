45b80659a018a54e9e6a69bffdae59dd29bfa176
SusanMackson <macksonebosetale@gmail.com>
2023-03-17 17:28:14 -0700

    task 6

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints lowercase in reverse
+ *
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i-()
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
