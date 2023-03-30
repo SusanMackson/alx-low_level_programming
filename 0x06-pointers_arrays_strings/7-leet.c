26a5e0fa0553b96fb84e32b3c6cb3b557fd1eed4
SusanMackson <macksonebosetale@gmail.com>
2023-03-30 08:21:01 -0700

    task 66.

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+char *leet(char *n)
+{
+	int m, s;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (m = 0; n[m] != '\0'; m++)
+	{
+		for (s = 0; s < 10; s++)
+		{
+			if (n[m] == s1[s])
+			{
+				n[m] = s2[s];
+			}
+		}
+	}
+	return (n);
+}
